6a81ff6770c7915ce677c84f025c5a966d24c65e
Randall <randall@merkadeus.com>
2017-12-13 23:33:47 -0600

    add investor register

--- app/src/js/components/Subscription/InvestorSteps/Step1.jsx
+++ app/src/js/components/Subscription/InvestorSteps/Step1.jsx
@@ -1,83 +1,225 @@
-import React from 'react';
+import React, { Component } from 'react';
+import autobind from 'react-autobind';
 import PropTypes from 'prop-types';
 import { Form } from 'semantic-ui-react';
 
 import InputField from '../../InputField';
 import Button from '../../Button';
 
-const Step1 = (props) => {
-  const inputFields = [{
-    id: 1,
-    placeholder: 'Nombre *',
-    hasError: false,
-    errorMessage: 'Campo requerido',
-    customClass: 'name',
-  },
-  {
-    id: 2,
-    placeholder: 'Apellido *',
-    hasError: false,
-    errorMessage: 'Campo requerido',
-  },
-  {
-    id: 3,
-    placeholder: 'Cédula *',
-    hasError: false,
-    errorMessage: 'Campo requerido',
-  },
-  {
-    id: 4,
-    placeholder: 'Teléfono *',
-    hasError: false,
-    errorMessage: 'Campo requerido',
-    inputType: 'tel',
-  },
-  {
-    id: 5,
-    placeholder: 'Teléfono adicional *',
-    hasError: false,
-    errorMessage: 'Campo requerido',
-    inputType: 'tel',
-  },
-  {
-    id: 6,
-    placeholder: 'Email *',
-    hasError: false,
-    errorMessage: 'Campo requerido',
-    inputType: 'email',
-    customClass: 'email',
-  }];
-
-  return (
-    <div className="investor-subscription step1">
-      <Form>
-        {
-          inputFields.map(inputField => (
-            <Form.Field key={inputField.id} className={inputField.customClass ? inputField.customClass : ''}>
-              <InputField
-                placeholder={inputField.placeholder}
-                hasError={inputField.hasError}
-                errorMessage={inputField.errorMessage}
-                inputType={inputField.inputType}
-              />
-            </Form.Field>
-          ))
-        }
-        <Button
-          onClick={props.btnOnClick}
-          text={props.btnText}
-          buttonType={props.btnType}
-        />
-        <span>Campos obligatorios **</span>
-      </Form>
-    </div>
-  );
-};
+class Step1 extends Component {
+  constructor(props) {
+    super(props);
+    const { onChangeField, investorInfo } = props;
+    this.state = {
+      isDisabled: true,
+      inputFields: [{
+        id: 1,
+        placeholder: 'Nombre *',
+        errorMessage: 'Campo requerido, digite solamente caracteres',
+        customClass: 'name',
+        onChangeField,
+        name: 'name',
+        defaultValue: investorInfo.name,
+        validation: (value) => {
+          if (value === '') return true;
+          if (/[a-zA-Z]/.test(value)) return false;
+          return true;
+        },
+      },
+      {
+        id: 2,
+        placeholder: 'Apellido *',
+        errorMessage: 'Campo requerido, digite solamente caracteres',
+        onChangeField,
+        name: 'lastName',
+        defaultValue: investorInfo.lastName,
+        validation: (value) => {
+          if (value === '') return true;
+          if (/[a-zA-Z]/.test(value)) return false;
+          return true;
+        },
+      },
+      {
+        id: 3,
+        placeholder: 'Cédula *',
+        errorMessage: 'Campo requerido. Formato de cédula 0-0000-0000',
+        onChangeField,
+        name: 'identification',
+        defaultValue: investorInfo.identification,
+        validation: (value) => {
+          if (value === '') return true;
+          if (/^([0-9]{1})-([0-9]{4})-([0-9]{4})$/.test(value)) return false;
+          return true;
+        },
+      },
+      {
+        id: 4,
+        placeholder: 'Teléfono *',
+        errorMessage: 'Campo requerido. Formato de teléfono 0000-00-00',
+        inputType: 'tel',
+        onChangeField,
+        name: 'phone',
+        defaultValue: investorInfo.phone,
+        validation: (value) => {
+          if (value === '') return true;
+          if (/^([0-9]{4})-([0-9]{2})-([0-9]{2})$/.test(value)) return false;
+          return true;
+        },
+      },
+      {
+        id: 5,
+        placeholder: 'Teléfono adicional *',
+        errorMessage: 'Campo requerido. Formato de teléfono 0000-00-00',
+        inputType: 'tel',
+        onChangeField,
+        name: 'aditionalPhone',
+        defaultValue: investorInfo.aditionalPhone,
+        validation: (value) => {
+          if (value === '') return true;
+          if (/^([0-9]{4})-([0-9]{2})-([0-9]{2})$/.test(value)) return false;
+          return true;
+        },
+      },
+      {
+        id: 6,
+        placeholder: 'Email *',
+        errorMessage: 'Campo requerido. Formato de email inválido.',
+        inputType: 'email',
+        customClass: 'email',
+        onChangeField,
+        name: 'email',
+        defaultValue: investorInfo.email,
+        validation: (value) => {
+          if (value === '') return true;
+          if (/^[a-z0-9](\.?[a-z0-9_-]){0,}@[a-z0-9-]+\.([a-z]{1,6}\.)?[a-z]{2,6}$/.test(value)) return false;
+          return true;
+        },
+      }],
+    };
+    autobind(this);
+  }
+  componentWillMount() {
+    const { investorInfo } = this.props;
+    const {
+      name,
+      lastName,
+      identification,
+      phone,
+      aditionalPhone,
+      email,
+    } = investorInfo;
+    if (
+      name !== '' ||
+      lastName !== '' ||
+      identification !== '' ||
+      phone !== '' ||
+      aditionalPhone !== '' ||
+      email !== ''
+    ) {
+      this.setButtonState(true);
+    }
+  }
+  componentDidMount() {
+    if (this.props.isComplete) this.setButtonState(false);
+  }
+  componentWillReceiveProps(nextProps) {
+    const { investorInfo } = nextProps;
+    const {
+      name,
+      lastName,
+      identification,
+      phone,
+      aditionalPhone,
+      email,
+    } = investorInfo;
+    if (
+      !this.state.inputFields[0].validation(name) &&
+      !this.state.inputFields[1].validation(lastName) &&
+      !this.state.inputFields[1].validation(identification) &&
+      !this.state.inputFields[1].validation(phone) &&
+      !this.state.inputFields[1].validation(aditionalPhone) &&
+      !this.state.inputFields[1].validation(email)
+    ) {
+      this.setButtonState(true);
+    } else if (
+      name === '' ||
+      lastName === '' ||
+      identification === '' ||
+      phone === '' ||
+      aditionalPhone === '' ||
+      email === ''
+    ) {
+      this.setButtonState(true);
+    } else {
+      this.setButtonState(false);
+    }
+  }
+  setButtonState(isDisabled) {
+    this.setState({
+      isDisabled,
+    });
+  }
+  handleButtonClick() {
+    const { investorInfo } = this.props;
+    const {
+      name,
+      lastName,
+      identification,
+      phone,
+      aditionalPhone,
+      email,
+    } = investorInfo;
+    if (
+      name !== '' ||
+      lastName !== '' ||
+      identification !== '' ||
+      phone !== '' ||
+      aditionalPhone !== '' ||
+      email !== ''
+    ) {
+      this.props.btnOnClick();
+    }
+  }
+  render() {
+    const { inputFields } = this.state;
+    return (
+      <div className="investor-subscription step1">
+        <Form>
+          {
+            inputFields.map(inputField => (
+              <Form.Field key={inputField.id} className={inputField.customClass ? inputField.customClass : ''}>
+                <InputField
+                  placeholder={inputField.placeholder}
+                  validation={inputField.validation}
+                  errorMessage={inputField.errorMessage}
+                  inputType={inputField.inputType}
+                  onChangeField={inputField.onChangeField}
+                  name={inputField.name}
+                  defaultValue={inputField.defaultValue}
+                />
+              </Form.Field>
+            ))
+          }
+          <Button
+            onClick={this.handleButtonClick}
+            text={this.props.btnText}
+            buttonType={this.props.btnType}
+            active={this.state.isDisabled}
+          />
+          <span>Campos obligatorios **</span>
+        </Form>
+      </div>
+    );
+  }
+}
 
 Step1.propTypes = {
   btnOnClick: PropTypes.func.isRequired,
   btnText: PropTypes.string.isRequired,
   btnType: PropTypes.string.isRequired,
+  onChangeField: PropTypes.func.isRequired,
+  investorInfo: PropTypes.object.isRequired,
+  isComplete: PropTypes.bool.isRequired,
 };
 
 export default Step1;
--- app/src/js/containers/InvestorRegister.jsx
+++ app/src/js/containers/InvestorRegister.jsx
@@ -1,80 +1,96 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 
-import { toggleMenuState, investorChangeCurrentStep } from '../actions';
+import * as GeneralActionCreators from '../actions';
+import * as RegisterInvestorActionCreators from '../actions/RegisterInvestorActionCreators';
 
 import Hero from '../components/Subscription/Hero';
 import SubscriptionFormInvestor from '../components/Subscription/SubscriptionForm/SubscriptionFormInvestor';
 
-const InvestorRegister = (props) => {
-  props.toggleMenuState(false);
-  let currentStep = 'uno';
-  switch (props.currentStep) {
-    case 1:
-      currentStep = 'uno';
-      break;
-    case 2:
-      currentStep = 'dos';
-      break;
-    default:
-      currentStep = 'uno';
+class InvestorRegister extends Component {
+  constructor(props) {
+    super(props);
+    const { dispatch } = props;
+    this.boundActionCreators = bindActionCreators({
+      GeneralActionCreators,
+      RegisterInvestorActionCreators,
+    }, dispatch);
   }
-  const step1 = {
-    ...props.step1,
-    handleClick: () => {
-      props.investorChangeCurrentStep(1);
-      console.log('Step 1 clicked!');
-    },
-    handleNextOnclick: () => {
-      props.investorChangeCurrentStep(2);
-    },
-  };
-  const step2 = {
-    ...props.step2,
-    handleClick: () => {
-      props.investorChangeCurrentStep(2);
-      console.log('Step 2 clicked!');
-    },
-    handleNextOnclick: () => {
-      console.log('SUCCESS');
-    },
-  };
-  return (
-    <div className="internal-page register investor">
-      <Hero
-        title="Invierta con nosotros"
-        content={
-          <div>
-            <p>La economía social genera oportunidades para el bienestar de la población.</p>
-            <p>El Club de Préstamos le ofrece una gran alternativa <span>para invertir con un mayor y mejor retorno mientras se transforman vidas,</span> proyectos y comunidades.</p>
-          </div>
-        }
-      />
-      <SubscriptionFormInvestor
-        currentStep={currentStep}
-        maxSteps="dos"
-        steps={{
-          step1,
-          step2,
-        }}
-      />
-    </div>
-  );
-};
+  componentWillMount() {
+    const { dispatch } = this.props;
+    dispatch(GeneralActionCreators.toggleMenuState(false));
+  }
+  render() {
+    const { dispatch, investorInfo } = this.props;
+    let currentStep = 'uno';
+    switch (this.props.currentStep) {
+      case 1:
+        currentStep = 'uno';
+        break;
+      case 2:
+        currentStep = 'dos';
+        break;
+      default:
+        currentStep = 'uno';
+    }
+    const investorInfoStepEvent = {
+      ...investorInfo,
+      step1: {
+        ...investorInfo.step1,
+        handleClick: () => {
+          dispatch(RegisterInvestorActionCreators.investorChangeCurrentStep(1));
+        },
+        handleNextOnclick: () => {
+          dispatch(RegisterInvestorActionCreators.investorChangeCurrentStep(2));
+          dispatch(RegisterInvestorActionCreators.investorIsCompletedStep(1));
+        },
+        onChangeField: (fieldChange) => {
+          dispatch(RegisterInvestorActionCreators.setInvestorInformation(fieldChange));
+        },
+      },
+      step2: {
+        ...investorInfo.step2,
+        handleClick: () => {
+          dispatch(RegisterInvestorActionCreators.investorChangeCurrentStep(2));
+        },
+        handleNextOnclick: () => {
+          console.log('SUCCESS');
+        },
+        onChangeField: (fieldChange) => {
+          dispatch(RegisterInvestorActionCreators.stepIsDisabled({ step: 'step2', isDisabled: fieldChange.isDisabled }));
+          dispatch(RegisterInvestorActionCreators.setInvestorInformation(fieldChange));
+        },
+      },
+    };
+    return (
+      <div className="internal-page register investor">
+        <Hero
+          title="Invierta con nosotros"
+          content={
+            <div>
+              <p>La economía social genera oportunidades para el bienestar de la población.</p>
+              <p>El Club de Préstamos le ofrece una gran alternativa <span>para invertir con un mayor y mejor retorno mientras se transforman vidas,</span> proyectos y comunidades.</p>
+            </div>
+          }
+        />
+        <SubscriptionFormInvestor
+          currentStep={currentStep}
+          maxSteps="dos"
+          investorInfo={investorInfoStepEvent}
+          captcha={this.props.captcha}
+        />
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   letterStep: state.investorSubscription.letterStep,
   currentStep: state.investorSubscription.currentStep,
-  step1: state.investorSubscription.step1,
-  step2: state.investorSubscription.step2,
+  investorInfo: state.investorSubscription,
+  captcha: state.recaptcha.captcha,
 });
 
-const mapDispatchToProps = dispatch =>
-  bindActionCreators({
-    toggleMenuState,
-    investorChangeCurrentStep,
-  }, dispatch);
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(InvestorRegister));
+export default withRouter(connect(mapStateToProps)(InvestorRegister));
