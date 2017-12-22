fb6a293b403f305d8dbb0ac1b0ffe00398c49377
Randall <randall@merkadeus.com>
2017-12-21 18:19:24 -0600

    add contact us fix

--- app/src/js/components/Home/Section/ScrollTo/index.jsx
+++ app/src/js/components/Home/Section/ScrollTo/index.jsx
@@ -1,27 +1,14 @@
-/* eslint  */
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Icon } from 'semantic-ui-react';
-
-const handleKeyDown = () => {};
 const ScrollTo = props => (
-  <div
-    role="button"
-    tabIndex={0}
-    className="scroll-icon"
-    onClick={props.handleScroll}
-    onKeyDown={handleKeyDown}
-  >
-    <div className="scroll-to">
-      <Icon size="small" name="chevron down" />
-    </div>
-    <p>Scroll Down</p>
+  <div className="scroll">
+    <a href={props.scrollTo}><span />Scroll To</a>
   </div>
 );
 
 ScrollTo.propTypes = {
-  handleScroll: PropTypes.func.isRequired,
+  scrollTo: PropTypes.string.isRequired,
 };
 
 export default ScrollTo;
