0ae89dda0cfb4fde5d3abb8cf6b27f8081518e0f
Randall <randall@merkadeus.com>
2018-02-15 21:24:29 -0600

    new builder

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
+import 'semantic-ui-css/semantic.min.css';
 import store, { history } from './store';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-import './style/index.scss';
+import './style/index.css';
 
 const target = document.querySelector('#app');
 
